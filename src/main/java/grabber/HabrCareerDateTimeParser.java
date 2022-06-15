f9c156439785dfd2954f34e7a5ae79bb5f4a23ed
VladislavCat <kshift84@gmail.com>
2022-06-16 00:17:28 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/grabber/HabrCareerDateTimeParser.java
+++ src/main/java/grabber/HabrCareerDateTimeParser.java
@@ -2,17 +2,12 @@ package grabber;
 
 import java.time.LocalDateTime;
 
-import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+import static java.time.format.DateTimeFormatter.ISO_ZONED_DATE_TIME;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse, ISO_OFFSET_DATE_TIME);
-    }
-
-    public static void main(String[] args) {
-        DateTimeParser hr = new HabrCareerDateTimeParser();
-        System.out.println(hr.parse("2022-06-15T15:24:03+03:00"));
+        return LocalDateTime.parse(parse, ISO_ZONED_DATE_TIME);
     }
 }
