db05647d5edbde8e8e95383ec901146c83cf872c
VladislavCat <kshift84@gmail.com>
2022-06-18 17:10:18 +0300

    SQLStore

--- src/main/java/grabber/PsqlStore.java
+++ src/main/java/grabber/PsqlStore.java
@@ -3,6 +3,7 @@ package grabber;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -55,10 +56,22 @@ public class PsqlStore implements Store, AutoCloseable {
     @Override
     public Post findById(int id) {
         Post rsl = null;
-        try (PreparedStatement ps = cn.prepareStatement("select * from vacancies where id = ?")) {
+        try (PreparedStatement ps = cn.prepareStatement("select * from vacancies where id_vacancies = ?")) {
+            ps.setInt(1, id);
             try (ResultSet rs = ps.executeQuery()) {
+                rs.next();
                 rsl = createPostOnResultSet(rs);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public boolean deleteAllPost() {
+        boolean rsl = false;
+        try (PreparedStatement ps = cn.prepareStatement("delete from vacancies")) {
+            rsl = ps.executeUpdate() != 0;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -75,5 +88,22 @@ public class PsqlStore implements Store, AutoCloseable {
         if (cn != null) {
             cn.close();
         }
+    }
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream("app.properties")) {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PsqlStore psqlStore = new PsqlStore(properties);
+        psqlStore.save(new Post("title1", "", LocalDateTime.now(), "yyyy"));
+        psqlStore.save(new Post("title2", "test2", LocalDateTime.now(), "xxxxx"));
+        psqlStore.save(new Post("title3", "test1", LocalDateTime.now(), "zzzzz"));
+        List<Post> allPost = psqlStore.getAll();
+        allPost.forEach(System.out::println);
+        System.out.println(psqlStore.findById(allPost.get(1).getId()));
+        psqlStore.deleteAllPost();
     }
 }
