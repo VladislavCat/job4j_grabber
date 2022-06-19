454ce8943a7dc7cf01fab63a821e797d70e21956
VladislavCat <kshift84@gmail.com>
2022-06-19 11:26:03 +0300

    2. SQL Select by company [#2760]

--- Itog.sql
+++ Itog.sql
@@ -1,8 +1,18 @@
-select p.name, c.name from person as p left join company as c on p.company_id = c.id where p.company_id != 5;
+select p.name, c.name
+from person as p
+left join company as c
+on p.company_id = c.id
+where p.company_id != 5;
 
 
-select c.name, count(c.id) from company as c right join person as p on c.id = p.company_id
+select c.name, count(c.id)
+from company as c
+right join person as p
+on c.id = p.company_id
 group by (c.name)
 having count(c.id) =
-(select MAX(count_id) from (select cc.name, count(cc.id) as count_id from company as cc right join person as pp on cc.id = pp.company_id group by(cc.name)) as count_id);
-
+(select count(company_id)
+from person
+group by company_id
+order by count(company_id) desc
+limit 1);
